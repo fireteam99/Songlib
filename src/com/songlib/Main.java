b51b201d523c6b113c14c0fe347331c18b5d3e5a
fireteam99 <raymancheungsy@gmail.com>
2020-02-09 15:25:45 -0500

    changed songlib to welcome to songlib

--- src/com/songlib/Main.java
+++ src/com/songlib/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 
-        primaryStage.setTitle("Songlib");
+        primaryStage.setTitle("Welcome to Songlib");
         primaryStage.setScene(new Scene(root, 750, 500));
         primaryStage.show();
     }
