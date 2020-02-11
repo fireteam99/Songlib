a971aca19739c4d8ec0e4e7b7a73fdfa3426fef5
Robert Cheng <rjc345@scarletmail.rutgers.edu>
2020-02-11 14:10:59 -0500

    added separate controllers for each fxml file

---
+++ src/com/songlib/CreateController.java
@@ -0,0 +1,37 @@
+package com.songlib;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateController {
+
+    @FXML
+    private Button cancelAddButton;
+
+    @FXML
+    private Button submitAddButton;
+
+    @FXML
+    private TextField nameTxtBox;
+
+    @FXML
+    private TextField artistTxtBox;
+
+    @FXML
+    private TextField albumTxtBox;
+
+    @FXML
+    private TextField yearTxtBox;
+
+    @FXML
+    void cancelAddingSong(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitNewSong(ActionEvent event) {
+
+    }
+}
---
+++ src/com/songlib/EditController.java
@@ -0,0 +1,40 @@
+package com.songlib;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class EditController {
+
+    @FXML
+    private TextField editNameBox;
+
+    @FXML
+    private TextField editArtistBox;
+
+    @FXML
+    private TextField editAlbumBox;
+
+    @FXML
+    private TextField editYearBox;
+
+    @FXML
+    private Button submitEdit;
+
+    @FXML
+    private Button cancelEdit;
+
+    @FXML
+    void cancelEditSong(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateSong(ActionEvent event) {
+
+    }
+
+}
+
